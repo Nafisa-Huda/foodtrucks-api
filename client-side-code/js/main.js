2833d65f05444bdd06bc7c509b9706f6ede77627
Nh0000 <94311576+Nh0000@users.noreply.github.com>
2022-06-06 13:31:25 -0400

    fixed another syntax error

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const state = document.querySelector('input').value
+    const stateName = document.querySelector('input').value
     try{
         const response = await fetch(`https://foodtruck-by-state-api.herokuapp.com/states/${stateName}`)
         const data = await response.json()
