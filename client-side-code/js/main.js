38df71f1af806ca8f2e1074a61ed4c9444d38768
Nh0000 <94311576+Nh0000@users.noreply.github.com>
2022-06-06 13:28:14 -0400

    fixed sytanx error

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const state = document.querySelector('input').value
     try{
-        const response = await fetch(`https://foodtruck-by-state-api.herokuapp.com/api/${stateName}`)
+        const response = await fetch(`https://foodtruck-by-state-api.herokuapp.com/states/${stateName}`)
         const data = await response.json()
 
         console.log(data)
